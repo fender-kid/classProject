b8e3bdd4aa71637a72fdcca76a3225da6d6665f0
fender-kid <jtt.sides@gmail.com>
2023-11-13 14:02:01 -0600

    Added ingredients to recipes.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,35 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://media.istockphoto.com/id/535786572/photo/grilled-striploin-steak.jpg?b=1&s=612x612&w=0&k=20&c=4MAcTyBrF7XkcltOt9WpTXwM6-uuf7qWUP6-j7srefc='),
-    new Recipe('Another Test Recipe', 'This is another test', 'https://media.istockphoto.com/id/535786572/photo/grilled-striploin-steak.jpg?b=1&s=612x612&w=0&k=20&c=4MAcTyBrF7XkcltOt9WpTXwM6-uuf7qWUP6-j7srefc='),
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+
+    new Recipe(
+      'Fatty Patty Burger',
+      'A Heart Attack in a Bun',
+      'https://upload.wikimedia.org/wikipedia/commons/4/4d/Cheeseburger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1),
+        new Ingredient('Cheese', 1),
+        new Ingredient('Pickles', 3),
+        new Ingredient('Lettuce', 1),
+        new Ingredient('Tomato', 1),
+        new Ingredient('Ketchup', 1),
+        new Ingredient('Mustard', 1),
+      ]),
   ];
 
   getRecipes() {
